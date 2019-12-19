c27753247b32f7aa3a0aa12d49e8bd369e623455
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2019-12-19 17:03:54 +0700

    Create Moreno77.sh

---
+++ Moreno77.sh
@@ -0,0 +1,20 @@
+clear
+echo "\n\n\n\033[1;32m      +---------[ Virus Maker by Moreno77 ]----------+"
+python .py
+sleep 1
+echo "\033[1;32m 1. Android"
+echo "\033[1;32m 2. Windows"
+echo "\033[1;32m 00. Exit"
+read -p " [?] Input => " pil;
+case $pil in
+1) cd .data
+sh 1.sh
+;;
+2) cd .data
+sh 2.sh
+;;
+00) exit
+;;
+esac
+done
+done
